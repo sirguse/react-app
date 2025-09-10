f13e3266dda3e557fb11a9434fc77f855de0f755
Sebastian Kazienko <sebastian.kazienko@marma.com.pl>
2025-09-10 14:13:50 +0200

    Shuffle

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import {useState} from 'react'
 
 
 
@@ -7,6 +9,14 @@ import CoreConcept from "./components/CoreConcept.jsx";
 
 
 function App() {
+  
+  let [selectedTopic, setSelectedTopic] = useState("Wybierz mnie")
+  function handleSelect(selectedButton) {
+    // console.log(selectedButton)
+
+    tabContent = selectedButton;
+    console.log(tabContent);
+  }
   return (
     <div>
       <main>
@@ -18,6 +28,17 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() =>handleSelect('components')} label="Components"></TabButton>
+            <TabButton onSelect={() =>handleSelect('jsx')} label="JSX"></TabButton>
+            <TabButton onSelect={() =>handleSelect('props')} label="Props"></TabButton>
+            <TabButton onSelect={() =>handleSelect('state')} label="State"></TabButton>
+          </menu>
+          {tabContent}
+
         </section>
           
       </main>
